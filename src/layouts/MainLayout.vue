4283f72fb9651acb0d4b85b81e0d998fac80df65
Elena <Polsh_el@mail.ru>
2024-04-27 20:16:19 +0300

    10_1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://img.icons8.com/ink/48/experimental-aquarium-ink.png">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay behavior="desktop" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://img.icons8.com/ink/48/experimental-aquarium-ink.png">
+          </q-avatar>
+          <div>Contact</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="https://img.icons8.com/ink/1600/experimental-aquarium-ink.png"
+      style="width: 200px; height: 200px">
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
